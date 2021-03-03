9299753802aeaf6f12ca731bc5c2d694df5482e3
Krishtheprogrammer <74716362+Krishtheprogrammer@users.noreply.github.com>
2021-03-03 18:36:38 +0530

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,25 @@
+class mango{
+    constructor(x,y,r){
+     var options={
+         isStatic:true,
+         friction:1
+     }
+     this.x=x;
+     this.y=y;
+     this.r=r;
+     this.image=loadImage("Mango.png");
+     this.body=Bodies.circle(this.x,this.y,this.r,options);
+     World.add(world,this.body);
+    }
+
+    display(){
+     var pos=this.body.position
+     push()
+     translate(pos.x,pos.y)
+     rotate(this.body.angle)
+     ellipseMode(CENTER)
+     imageMode(CENTER)
+     image(this.image,0,0,this.r*2,this.r*2)
+     pop()
+    }
+}
